9cbfb63b3bdbe079dd036b39517e3d188af98577
lucas.lvn <lucas.lvn97439@gmail.com>
2023-02-22 15:42:20 +0100

    Lors du chargement des models via fetch vérifie si _models est bien définie et qu'il n'est pas = null

--- src/components/select_model.tsx
+++ src/components/select_model.tsx
@@ -1,13 +1,21 @@
-export default function SelectModel(){
-    const getModels = async () => {
+import { createResource, For } from "solid-js";
+import { models, setModels } from "~/store/signaux"
 
+const fetchModels = async () => (await fetch(`http://localhost:8000/api/models`)).json();
+
+export default function SelectModel(){
+    const [_models] = createResource(fetchModels)
+
+    if (_models() !== undefined && _models() !== null) {
+      setModels(_models())
     }
 
-    return <select id="countries" class="mx-auto w-6/12 bg-indigo-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    const setSelectedModel = (e: any) => {}
+
+    return <select onChange={setSelectedModel} id="models" class="mx-auto w-64 bg-indigo-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option selected>Aucun model sélectionner</option>
-        <option value="mobilnet">Mobilnet</option>
-        <option value="xception">Xception</option>
-        <option value="vgg16">vgg16</option>
-        <option value="cnn">CNN from skratch</option>
+        <For each={_models()}>{(model, i) =>
+            <option value={model.id}>{model.name}</option>
+        }</For>
     </select>
 }
