07238324dbb0534b19d2bea562aa7022efc7ed77
lucas.lvn <lucas.lvn97439@gmail.com>
2023-02-22 04:41:53 +0100

    Ajout d'un système de notification dynamique

--- src/root.tsx
+++ src/root.tsx
@@ -1,10 +1,17 @@
 // @refresh reload
-import { Suspense } from "solid-js";
+import { createSignal, For, onMount, Suspense } from "solid-js";
 import { useLocation, A, Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title } from "solid-start";
 
 import "./root.css";
 
+import {user, setUser} from "./store/signaux";
+import Notification from "./components/notification";
+import { notifs, pushNotif } from "./store/signaux";
+
 export default function Root() {
+  onMount(() => {
+    // pushNotif({message: "test dynamique", color:"blue"})
+  })
   
   return (
     <Html lang="en">
@@ -14,10 +21,14 @@ export default function Root() {
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <Body class="bg-indigo-700">
+      <Body class="bg-indigo-700" id="main">
         <div class="w-full text-center text-xl flex justify-center items-center" style="height: 64px">
           <A href="/" class="text-center text-white tracking-[.20em]">ClassiFR</A>
         </div>
+
+        <For each={notifs()}>{(notif, i) => 
+          <Notification notif={notif} class=""/>
+        }</For>
 
         <Suspense>
           <ErrorBoundary>
