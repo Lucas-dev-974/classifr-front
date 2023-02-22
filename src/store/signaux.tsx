493c05c6b700f556f1d6d1dc002d579e479c1d97
lucas.lvn <lucas.lvn97439@gmail.com>
2023-02-22 04:39:19 +0100

    ajout des signaux

---
+++ src/store/signaux.tsx
@@ -0,0 +1,28 @@
+import { createEffect } from 'solid-js';
+import { render } from 'solid-js/web';
+import { createStore, Store, SetStoreFunction } from 'solid-js/store';
+
+export const [user, setUser]                  = createStore({})
+export const [modelSetting, setModelSettings] = createStore({})
+
+const [_notif, setNotif] = createStore([])
+
+export const notifs = () => {
+    return _notif
+}
+
+export const  pushNotif = (__notif: any) => {
+    const notif = {
+        id:      notifs().length + 1,
+        message: __notif.message,
+        color:   __notif.color
+    }
+
+    
+    setNotif([...notifs(), notif]);
+}
+
+export const removeNotif = (notifid: number) => {
+    console.log('okokok', notifid)
+    setNotif((items: any) => items.filter((item: any) => item.id !== notifid));
+} 
