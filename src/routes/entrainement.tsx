a8fc9f2921c89b923cf244f0cb085902bfa0eb0f
lucas.lvn <lucas.lvn97439@gmail.com>
2023-02-23 12:34:35 +0100

    code review

--- src/routes/entrainement.tsx
+++ src/routes/entrainement.tsx
@@ -1,6 +1,7 @@
 import { createResource, For, onMount } from "solid-js"
 import ChooseClasses from "~/components/train_add_classes"
 import SelectModel from "~/components/select_model"
+import AddModel from "~/components/add_model";
 
 import { trainClassesSelect } from "~/store/signaux";
 
@@ -11,7 +12,7 @@ export default function entrainement(){
 
     }
 
-    return <main class="pt-4 sm:container text-white mx-auto">
+    return <main class="pt-4 sm:container xl:px-80 lg:px-40 text-white mx-auto">
 
         <div class="px-10">
             <div class="flex items-center">
@@ -28,8 +29,8 @@ export default function entrainement(){
 
         <div class="px-10 mt-10">
             <div class="flex items-center">
-                <h3>Sélectionner un modèle</h3>
-                <ChooseClasses />
+                <h3>Sélectionner un modèle / ajouter</h3>
+                <AddModel />
             </div>                
             <SelectModel />
         </div> 
@@ -53,7 +54,7 @@ export default function entrainement(){
 
                 <div class="w-full flex my-2">
                     <p class="mx-3" style="width: 150px">Data augmentation</p>
-                    <input type="checkbox" />
+                    <input type="checkbox" class="  mt-[0.15rem] mr-[6px]  h-[1.125rem] w-[1.155rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-[rgba(0,0,0,0.25)] bg-white outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:bg-white focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent" />
                 </div>
             </div>              
         </div>    
@@ -65,9 +66,8 @@ export default function entrainement(){
                 <p>Feedback de l'entrainement</p>
             </div>              
         </div>    
-        <div class="flex juitify-center w-full mx-auto absolute">
-            <button type="button" onClick={train} class="w-64 justify-center text-white  bg-[#7D6ADE] font-medium rounded-lg text-sm px-5 py-2.5 mt-4">Entrainer</button>
+        <div class="flex juitify-center w-full">
+            <button type="button" onClick={train} class="w-64   bg-[#7D6ADE] rounded-lg text-sm px-5 py-2.5 mt-4 mx-auto">Entrainer</button>
         </div>
     </main>
 }
-const ar: Array<number> = []
--- src/routes/prediction.tsx
+++ src/routes/prediction.tsx
@@ -81,7 +81,7 @@ export default function Prediction() {
                     <div class="flex items-center space-x-2 mx-auto" id="selected_img">
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#929292" d="M7 17h10q.3 0 .45-.275t-.05-.525l-2.75-3.675q-.15-.2-.4-.2t-.4.2L11.25 16L9.4 13.525q-.15-.2-.4-.2t-.4.2l-2 2.675q-.2.25-.05.525T7 17Zm-2 4q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Z"/></svg>
                     </div>
-                    <input type="file" name="file_upload" class="hidden" accept="image/png, image/jpg, image/jpeg " onChange={handleImage}/>
+                    <input type="file" name="file_upload"  class="hidden" accept="image/png, image/jpg, image/jpeg " onChange={handleImage}/>
                 </label>
             </div>
 
