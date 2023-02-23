870902c26f99e4808d049d51cd44c6c1b8647ee6
lucas.lvn <lucas.lvn97439@gmail.com>
2023-02-23 12:34:19 +0100

    code review

--- src/components/train_add_classes.tsx
+++ src/components/train_add_classes.tsx
@@ -8,54 +8,48 @@ const fetchClasses = async () => (await fetch(`http://localhost:8000/api/classes
 export default function ChooseClasses(){
     const [classes] = createResource(fetchClasses);
 
+
+    /**
+     * Cette fonction as pour but de sélectionner/désélectionner une classes et l'ajouter au Store trainClassesSelect
+     * @param {event} classe
+     */
     const selectClasse = (classe: any) => {
         classe = JSON.parse(classe.target.value)
-        if(trainClassesSelect.map(clas => clas.id == classe.id).includes(true)){
-            let _classes = trainClassesSelect.filter(clas => clas.id != classe.id)
-            setTrainClassesSelect(_classes)
-        }
-        else{
-            setTrainClassesSelect([...trainClassesSelect, classe])  
-        }
+        const classe_ever_selected = trainClassesSelect.map(clas => clas.id == classe.id).includes(true)
         
+        if(classe_ever_selected) setTrainClassesSelect(trainClassesSelect.filter(clas => clas.id != classe.id))
+        else setTrainClassesSelect([...trainClassesSelect, classe])  
     }
+
+
     return <>
-        <button type="button" class="inline-block rounded px-2 py-2" data-te-toggle="modal" data-te-target="#exampleModalCenter" data-te-ripple-init data-te-ripple-color="light">
+        <button type="button"  data-te-toggle="modal" data-te-target="#select_classes_to_train"  >
             <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path fill="#32a64b" d="M18 12.998h-5v5a1 1 0 0 1-2 0v-5H6a1 1 0 0 1 0-2h5v-5a1 1 0 0 1 2 0v5h5a1 1 0 0 1 0 2z"/></svg>
         </button>
-        <div data-te-modal-init class="fixed top-0 left-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog">
+        <div data-te-modal-init class="fixed top-0 left-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none" id="select_classes_to_train" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog">
             <div data-te-modal-dialog-ref class="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]">
-                <div class="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
-                <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-                    <h5 class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200" id="exampleModalScrollableLabel">
-                        Sélectionner 
-                    </h5>
-                    <button type="button" class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none" data-te-modal-dismiss aria-label="Close">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                    </button>
-                </div>
-                <div class="relative p-4">
+                <div class="pointer-events-auto  flex w-full flex-col rounded-md border-none bg-[#4544AB] ">
+                    <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+                        <h5 class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200" id="exampleModalScrollableLabel">
+                            Sélectionner 
+                        </h5>
+                    </div>
+                    <div class="relative p-4 px-10">
 
-                    <For each={classes()}>{(classe, i) => 
-                        <div class="flex justify-around">
-                            <p style="width: 150px">{classe.name}</p>
-                            <input type="checkbox" onClick={selectClasse} value={JSON.stringify(classe)}/>
-                        </div>
-                    }</For>
+                        <For each={classes()}>{(classe, i) => 
+                            <div class="flex justify-around border-b py-2 ">
+                                <p style="width: 150px">{classe.name}</p>
+                                <input type="checkbox" onClick={selectClasse} value={JSON.stringify(classe)}   class="mt-[0.15rem] mr-[6px]  h-[1.125rem] w-[1.155rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-[rgba(0,0,0,0.25)] bg-white outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:bg-white focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent" />
+                            </div>
+                        }</For>
 
 
-                </div>
-                <div class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-                    <button type="button" class="inline-block rounded bg-primary-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200" data-te-modal-dismiss data-te-ripple-init data-te-ripple-color="light">
-                    Close
-                    </button>
-
-                    <button type="button" class="ml-1 inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]" data-te-ripple-init data-te-ripple-color="light">
-                    Save changes
-                    </button>
-                </div>
+                    </div>
+                    <div class="flex flex-shrink-0 flex-wrap items-center justify-center rounded-b-md  border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+                        <button type="button" class="inline-block rounded bg-primary-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200" data-te-modal-dismiss data-te-ripple-init data-te-ripple-color="light">
+                            Suivant
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
