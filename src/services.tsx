b5486a2eb600aaad5b39ef4e28f213d6611da9cf
lucas.lvn <lucas.lvn97439@gmail.com>
2023-02-27 05:34:04 +0100

    mise en place d'un servie request

--- src/services.tsx
+++ src/services.tsx
@@ -1,11 +1,20 @@
 export const Formater = (data: any) => {
     const formdata = new FormData()
-
-    Object.keys(data).forEach((_data, i) => {
-        console.log(_data, i)
-        formdata.append(_data, data[_data])
-    })
-
+    Object.keys(data).forEach(_data => formdata.append(_data, data[_data]) )
     return formdata
 }
 
+export const request = async (url: string, method: string, data: any) => {
+
+    const host = 'http://localhost:8000/'
+
+    const response = await fetch(host + url, {
+        method: method,
+        body: data
+    })
+
+    return {
+        status: response.status,
+        json: await response.json()
+    }
+}
