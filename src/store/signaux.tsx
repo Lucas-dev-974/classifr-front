8fb4e2867b32bde231881a24d27637780dfef835
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-03 05:16:07 +0100

    ajout de ka méthode selectClasse + code review

--- src/store/signaux.tsx
+++ src/store/signaux.tsx
@@ -1,35 +1,37 @@
-import { createEffect } from 'solid-js';
-import { render } from 'solid-js/web';
-import { createStore, Store, SetStoreFunction } from 'solid-js/store';
+import { createStore } from 'solid-js/store';
+import { createEffect, onMount } from 'solid-js';
 
-export const [user, setUser]                  = createStore({})
-export const [modelSetting, setModelSettings] = createStore({})
+const [user, setUser]       = createStore({})
+const [_notif, setNotif]    = createStore([])
 
-const [_notif, setNotif] = createStore([])
+export const [classes, setClasses] = createStore([])
+export const [trainClassesSelect, setTrainClassesSelect] = createStore([]);
 
-export const notifs = () => {
-    return _notif
-}
+export const [models, setModels] = createStore([])
+export const [selectedModel, setSelectedModel] = createStore()
 
-export const  pushNotif = (__notif: any) => {
+// Functions
+export const notifs    = () => { return _notif }
+export const pushNotif = (__notif: any) => {
     const notif = {
         id:      notifs().length + 1,
         message: __notif.message,
         color:   __notif.color ?? 'blue'
     }
-
     
     setNotif([...notifs(), notif]);
 }
+export const removeNotif = (notifid: number) => setNotif((items: any) => items.filter((item: any) => item.id !== notifid));
 
-export const removeNotif = (notifid: number) => {
-    console.log('okokok', notifid)
-    setNotif((items: any) => items.filter((item: any) => item.id !== notifid));
-} 
+// Models
+// export const pushModel   = (model: Object) => {}
 
-// Classes
-export const [trainClassesSelect, setTrainClassesSelect] = createStore([]);
 
-// Models 
-export const [models, setModels] = createStore([])
-export const [selectedModel, setSelectedModel] = createStore({})
+export const selectClasse = (classe: any) => {
+    classe = JSON.parse(classe.target.value)
+    const classe_ever_selected = trainClassesSelect.map(clas => clas.id == classe.id).includes(true)
+    
+    if(classe_ever_selected) setTrainClassesSelect(trainClassesSelect.filter(clas => clas.id != classe.id))
+    else setTrainClassesSelect([...trainClassesSelect, classe])  
+}
+
