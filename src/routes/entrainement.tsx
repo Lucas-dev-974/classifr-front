28334bd1e7db72d0f27ff7c72222bd123c522a27
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-03 05:10:32 +0100

    code review

--- src/routes/entrainement.tsx
+++ src/routes/entrainement.tsx
@@ -8,15 +8,13 @@ import { trainClassesSelect } from "~/store/signaux";
 
 
 export default function entrainement(){
-    const train = () => {
+    const train = () => {}
 
-    }
+    return <main class="pt-[5rem] sm:w-3/5 xl:px-60 lg:px-40  text-white mx-auto">
 
-    return <main class="pt-4 sm:container xl:px-80 lg:px-40 text-white mx-auto">
-
-        <div class="px-10">
+        <div class="md:py-10 sm:py-5 py-2">
             <div class="flex items-center">
-                <h3>Sélectionner sur quel classe entrainer </h3>
+                <h3 class="text-lg md:text-2xl">Sélectionner sur quel classe entrainer </h3>
                 <ChooseClasses />
             </div>
 
@@ -27,41 +25,40 @@ export default function entrainement(){
             </div>
         </div>    
 
-        <div class="px-10 mt-10">
+        <div class="md:py-10 sm:py-5 py-2">
             <div class="flex items-center">
-                <h3>Sélectionner un modèle / ajouter</h3>
+                <h3 class="text-lg md:text-2xl">Sélectionner un modèle / ajouter</h3>
                 <AddModel />
             </div>                
             <SelectModel />
         </div> 
 
-        <div class="px-10 mt-10">
-            <h3>Paramètre d'entrainement</h3>
+        <div class="md:py-10 sm:py-5 py-2">
+            <h3 class="text-lg md:text-2xl">Paramètre d'entrainement</h3>
 
             <div id="model-settings">
                 <div class="w-full flex my-2">
-                    <p class="mx-3" style="width: 150px">Learning-rate</p>
+                    <p class="mx-3" style="width: 120px">Learning-rate</p>
                     <input type="number" class="p-1 text-zinc-900" />
                 </div>
                 <div class="w-full flex my-2">
-                    <p class="mx-3" style="width: 150px">Dropout</p>
+                    <p class="mx-3" style="width: 120px">Dropout</p>
                     <input type="number" class="p-1 text-zinc-900" />
                 </div>
                 <div class="w-full flex my-2">
-                    <p class="mx-3" style="width: 150px">Epoques</p>
+                    <p class="mx-3" style="width: 120px">Epoques</p>
                     <input type="number" class="p-1 text-zinc-900"  name="epoques"/>
                 </div>
 
                 <div class="w-full flex my-2">
-                    <p class="mx-3" style="width: 150px">Data augmentation</p>
+                    <p class="mx-3" style="width: 120px">Data Aug</p>
                     <input type="checkbox" class="  mt-[0.15rem] mr-[6px]  h-[1.125rem] w-[1.155rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-[rgba(0,0,0,0.25)] bg-white outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:bg-white focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent" />
                 </div>
             </div>              
         </div>    
 
-        <div class="px-10 mt-10">
-            <h3>Paramètre d'entrainement</h3>
-
+        <div class="md:py-10 sm:py-5 py-2">
+            <h3 class="text-lg md:text-2xl">Paramètre d'entrainement</h3>
             <div id="model-settings" class="ml-3 bg-slate-800 p-5 rounded">
                 <p>Feedback de l'entrainement</p>
             </div>              
